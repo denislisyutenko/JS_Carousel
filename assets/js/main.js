7a727027037297b2bbbaac0d26638509a29c08dd
Denis Lisyutenko <denislisyutenko@gmail.com>
2023-02-14 10:52:17 +0200

    Created a style for the carousel

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,5 @@
+import SwipeCarousel from './swipe-carousel.js';
 
-// import Carousel from './carousel.js'; 
-import SwipeCarousel from './swipe-carousel.js'; 
-
-// const carousel = new SwipeCarousel('#carousel', '.slide', 1000); верная
-const carousel = new SwipeCarousel({
-    containerID: '#carousel',
-    slideID: '.slide',
-    interval: 1000,
-    isPlaying: false
-});
+const carousel = new SwipeCarousel();
 
 carousel.init();
-
-
-
-
-
--- assets/js/swipe-carousel.js
+++ assets/js/swipe-carousel.js
@@ -1,22 +1,21 @@
-import Carousel from "./carousel.js";
+import Carousel from './carousel.js';
 
-class SwipeCarousel extends Carousel { 
-    _initListeners () {
-        super._initListeners();
-        this.container.addEventListener('touchstart', this._swipeStart.bind(this));
-        this.container.addEventListener('touchend', this._swipeEnd.bind(this));
-    }
-    
-    _swipeStart (e) {
-        this.swipeStartX = e.changedTouches[0].pageX;
-    }
-    
-    _swipeEnd (e) {
-        this.swipeEndX = e.changedTouches[0].pageX;
-        this.swipeStartX - this.swipeEndX < -50 && this.prev();
-        this.swipeStartX - this.swipeEndX > 50 && this.next();
-    }
+class SwipeCarousel extends Carousel {
+  _initListeners() {
+    super._initListeners();
+    this.container.addEventListener('touchstart', this._swipeStart.bind(this));
+    this.container.addEventListener('touchend', this._swipeEnd.bind(this));
+  }
+
+  _swipeStart(e) {
+    this.swipeStartX = e.changedTouches[0].pageX;
+  }
+
+  _swipeEnd(e) {
+    this.swipeEndX = e.changedTouches[0].pageX;
+    this.swipeStartX - this.swipeEndX < -50 && this.prev();
+    this.swipeStartX - this.swipeEndX > 50 && this.next();
+  }
 }
 
 export default SwipeCarousel;
-
